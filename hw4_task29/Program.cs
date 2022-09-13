a383e16ef170c601d2a30847ce2a63e10ca1130b
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-13 11:37:30 +0300

    Solution task29

---
+++ hw4_task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. 
+// Данные вводятся с консоли пользователем
+
+void CreateArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine("Введите следующее число массива");
+        int arrayNumber = Convert.ToInt32(Console.ReadLine());
+        array[index] = arrayNumber;
+        index++;
+    }
+}
+
+Console.WriteLine("Задайте размер массива");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[arraySize];
+
+CreateArray(Array);
+
+Console.Write("Задан массив: ");
+Console.WriteLine(string.Join(", ", Array));
