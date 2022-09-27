09e94acb7008e30e2bbd0445718f40e4498ebc9a
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-27 22:28:20 +0300

    Solution task64

---
+++ hw9_task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumberNto1 (int n)
+{
+    if (n <= 0) return;
+    else
+    {
+        Console.Write (n + " ");
+        PrintNumberNto1(n - 1);
+    }
+}
+int number = ReadNumber("Введите N");
+PrintNumberNto1(number);
