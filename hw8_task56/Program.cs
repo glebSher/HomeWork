b3ab544b91d9e804ab2239f9d0b9e06c6627dd18
glebshcherbakov <gleb.sher309@gmail.com>
2022-09-30 19:58:37 +0300

    Solution task56

---
+++ hw8_task56/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт 
+// номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadNumber(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount,
+                  int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rowsCount, columnsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinSumElementRow(int[,] matr)
+{
+    int minRows = 0;
+    int sumElement = 0;
+    int minSumElement = sumElement;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            sumElement = sumElement + matr[i, j];
+        }
+        Console.WriteLine($"Сумма элементов строки {i} = {sumElement}");
+        if (sumElement < minSumElement)
+        {
+            minSumElement = sumElement;
+            minRows = i;
+        }
+        sumElement = 0;
+    }
+    Console.WriteLine($"Наименьшая сумма элементов: {minSumElement}");
+    Console.WriteLine($"Строка с наименьшей суммой элементов: {minRows}");
+}
+
+int m = ReadNumber("Введите количество строк:");
+int n = ReadNumber("Введите количество столбцов:");
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+MinSumElementRow(matrix);
